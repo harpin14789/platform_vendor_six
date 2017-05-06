voidz777-pure/android_vendor_pure

products: add sailfish product

vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo angler-userdebug
 add_lunch_combo bullhead-userdebug
 add_lunch_combo marlin-userdebug
+add_lunch_combo sailfish-userdebug
 add_lunch_combo shamu-userdebug

harpin14789/platform_vendor_six

vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
 add_lunch_combo marlin-user
+add_lunch_combo sailfish-user
 add_lunch_combo shamu-user