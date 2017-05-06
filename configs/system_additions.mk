voidz777-pure/android_vendor_pure

Add sysinit script. *also remove logwrapper *add system init service to init.purenexus.rc

configs/system_additions.mk
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# init.d script support
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/bin/sysinit:system/bin/sysinit
+
+# PureNexus-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/etc/init.purenexus.rc:root/init.purenexus.rc
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/nexus/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \

harpin14789/platform_vendor_six

configs/system_additions.mk
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# init.d script support
+PRODUCT_COPY_FILES += \
+    vendor/pure/prebuilt/bin/sysinit:system/bin/sysinit
+
+# Pure-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/pure/prebuilt/etc/init.purenexus.rc:root/init.purenexus.rc
+
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1