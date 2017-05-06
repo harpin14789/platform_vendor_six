YAOSP-NG/vendor_yaosp

add shamu product, and stock bootanimations

vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo nexus_angler-user
 add_lunch_combo nexus_bullhead-user
+add_lunch_combo nexus_shamu-user

harpin14789/platform_vendor_six

vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
+add_lunch_combo shamu-user