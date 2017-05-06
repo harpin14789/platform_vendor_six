voidz777-pure/android_vendor_pure

vendor: add camera effects prebuilts Pure Edits: *only install media effects on devices with no vendor partition

configs/aosp_fixes.mk
@@ -31,3 +31,10 @@ else
 PRODUCT_COPY_FILES += \
     vendor/pure/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
 endif
+
+# Camera Effects for devices without a vendor partition
+ifneq ($(filter pure_shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES +=  \
+    vendor/pure/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/pure/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+endif

harpin14789/platform_vendor_six

configs/aosp_fixes.mk
@@ -22,3 +22,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dataroaming=false
+
+# Camera Effects for devices without a vendor partition
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES +=  \
+    vendor/pure/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/pure/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+endif