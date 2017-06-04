153e927472ba4b1c9cb25793bb601817f7ea8548
liquid0624 <rjbirnesser@gmail.com>
2017-06-04 00:45:12 -0400

    vendor: build file name suffix official

--- configs/version.mk
+++ configs/version.mk
@@ -14,7 +14,7 @@
 
 #SiX versioning
 ifndef SIX_BUILD_TYPE
-    SIX_BUILD_TYPE := OMS.v2.8-USER
+    SIX_BUILD_TYPE := OMS.v2.8-OFFICIAL
 endif
 
 SIX_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(SIX_BUILD_TYPE)
